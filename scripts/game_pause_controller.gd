39a3c650183dfbc3c38012e5877b24ac907aff76
Artin <sheikhartin@gmail.com>
2025-01-28 10:05:17 +0330

    To prevent displaying after the end of the game

--- scripts/game_pause_controller.gd
+++ scripts/game_pause_controller.gd
@@ -2,15 +2,23 @@ extends Node2D
 
 @onready var pause_menu_layout: VBoxContainer = $PauseMenuLayout
 
+var game_ended: bool = false
+
 
 func _ready() -> void:
 	pause_menu_layout.visible = false
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if not event.is_action_pressed("toggle_pause"):
+	if game_ended or not event.is_action_pressed("toggle_pause"):
 		return
 
 	var paused_state: bool = not get_tree().is_paused()
 	get_tree().paused = paused_state
 	pause_menu_layout.visible = paused_state
+
+
+func set_game_ended(ended: bool) -> void:
+	game_ended = ended
+	if game_ended:
+		pause_menu_layout.visible = false
