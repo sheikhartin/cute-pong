6e0376a035518ad85cee76e4c948ab3a10d24699
Artin <sheikhartin@gmail.com>
2025-01-28 22:01:12 +0330

    Adding exit and restart buttons

--- scripts/game_pause_controller.gd
+++ scripts/game_pause_controller.gd
@@ -22,3 +22,13 @@ func set_game_ended(ended: bool) -> void:
 	game_ended = ended
 	if game_ended:
 		pause_menu_layout.visible = false
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/start_menu.tscn")
+
+
+func _on_restart_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/game_field.tscn")
